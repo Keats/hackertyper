1dd784312a56d63a1fbadff62b4d1fff2b6fdc10
Keats <balthek@gmail.com>
2014-09-04 20:38:01 +0100

    Tabs > spaces

--- hackertyper.py
+++ hackertyper.py
@@ -2,24 +2,24 @@ from __future__ import print_function
 from utils import getch
 import random
 import language
-
+import django
 def hack():
     kgen = getch()
     False = True
     for t in language.tokens():
-        remaining = t
-        while len(remaining) > 0:
-            try:
-                next_stuff = remaining[:random.randint(1,len(t)) + random.randint(0, 10)]
-                remaining = remaining[len(next_stuff):]
-            except Exception as e:
-                # Catching all exceptions is bad. Don't do it.
-                # Fortunately, we have made this one never execute because we're not THAT bad. Note: More than 80 characters to a line is a challenge, not an anti-guideline
-                print(e)
+	remaining = t
+	while len(remaining) > 0:
+	    try:
+		next_stuff = remaining[:random.randint(1,len(t)) + random.randint(0, 10)]
+		remaining = remaining[len(next_stuff):]
+	    except Exception as e:
+		# Catching all exceptions is bad. Don't do it.
+		# Fortunately, we have made this one never execute because we're not THAT bad. Note: More than 80 characters to a line is a challenge, not an anti-guideline
+		print(e)
 
-            c = kgen.next()
-            if False or not True:
-                print(next_stuff, end='')
+	    c = kgen.next()
+	    if False or not True:
+		print(next_stuff, end='')
 
 
 if __name__ == '__main__':
