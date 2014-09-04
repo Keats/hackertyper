b26036bc2c61d732dc80b54e0bd333a7b39acb47
Damian Moore <damian@epixstudios.co.uk>
2014-09-04 20:30:19 +0100

    Markov model

--- language.py
+++ language.py
@@ -2,6 +2,7 @@ import os
 import random
 import re
 import sys
+from collections import defaultdict, Counter
 
 def index_project(path):
     file_list = []
@@ -14,15 +15,24 @@ def index_project(path):
 
 
 def tokens():
-    py_files = index_project('django')
+    model = create_markov_model()
+    tok = None
     while True:
-        filename = random.choice(py_files)
-        for token in tokens_from_file(filename):
-            yield token
+        choices = model[tok]
+        if not choices:
+            tok = None
+            continue
+        nexttok = random.choice(choices)
+        if nexttok == tok:
+            tok = None
+            continue
+        tok = nexttok
+        yield tok
 
 
 def tokens_from_file(path):
     with open(path) as f:
+        return f.readlines()
         return re.findall(
             r'\w+|\s+|'
             "'.*?'"
@@ -30,6 +40,17 @@ def tokens_from_file(path):
             r'|[^#]|#[^\n]*', f.read())
 
 
+def create_markov_model():
+    py_files = index_project('django')
+    bigrams = defaultdict(list)
+    for file in py_files:
+        tokens = tokens_from_file(file)
+        for word1, word2 in zip([None] + tokens, tokens):
+            bigrams[word1].append(word2)
+    return bigrams
+
+
 if __name__ == '__main__':
     for t in tokens():
         sys.stdout.write(t)
+#    print create_markov_model()
