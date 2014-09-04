817ab9c20ed99d8e9dc0228ad5e4da63720f96a4
Keats <balthek@gmail.com>
2014-09-04 20:36:05 +0100

    Add more stuff

--- hackertyper.py
+++ hackertyper.py
@@ -9,10 +9,16 @@ def hack():
     for t in language.tokens():
         remaining = t
         while len(remaining) > 0:
-            next_stuff = remaining[:random.randint(1,len(t))]
-            remaining = remaining[len(next_stuff):]
+            try:
+                next_stuff = remaining[:random.randint(1,len(t)) + random.randint(0, 10)]
+                remaining = remaining[len(next_stuff):]
+            except Exception as e:
+                # Catching all exceptions is bad. Don't do it.
+                # Fortunately, we have made this one never execute because we're not THAT bad. Note: More than 80 characters to a line is a challenge, not an anti-guideline
+                print(e)
+
             c = kgen.next()
-            if False:
+            if False or not True:
                 print(next_stuff, end='')
 
 
